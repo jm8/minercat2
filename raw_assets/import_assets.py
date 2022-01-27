a2bafb84d334a9709982b6fb28b0f17c8105ce40
jm8 <joshua.sims@hilliardschools.org>
2022-01-27 15:53:34 -0500

    renamed / copied assets

--- raw_assets/import_assets.py
+++ raw_assets/import_assets.py
@@ -1,11 +1,11 @@
 import json
-import os
+import shutil
 project = json.load(open("project.json"))
 
 for target in project["targets"]:
     if target["name"] != 'Visuals': continue
     for costume in target["costumes"]:
         try:
-            os.rename(f"minercat2_assets/{costume['md5ext']}", f"minercat2_assets_renamed/{costume['name']}.{costume['dataFormat']}")
+            shutil.copy2(f"minercat2_assets/{costume['md5ext']}", f"minercat2_assets_renamed/{costume['name']}.{costume['dataFormat']}")
         except FileNotFoundError:
             print(f"NOT FOUND: {costume['name']}")
