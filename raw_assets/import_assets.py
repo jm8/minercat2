ac4d88cf9e7f5258e442c3e27394291e3cb763cc
jm8 <joshua.sims@hilliardschools.org>
2022-02-10 15:43:32 -0500

    organized the assets into folders

--- raw_assets/import_assets.py
+++ raw_assets/import_assets.py
@@ -1,11 +1,41 @@
 import json
+import os
 import shutil
 project = json.load(open("project.json"))
 
+prefixes = [
+    'letter',
+    'armor',
+    'backpack',
+    'block',
+    'crack',
+    'letter',
+    'pickaxe',
+    'shop',
+]
+
+def get_prefix(name):
+    for prefix in prefixes:
+        if name.startswith(prefix):
+            return prefix
+
+    return 'other'
+    
+
 for target in project["targets"]:
-    if target["name"] != 'Visuals': continue
+    if "costumes" not in target: continue
+    #if target["name"] != 'Visuals': continue
+    if target["name"] != 'Visuals':
+        folder = target["name"]
+    else:
+        folder = get_prefix(target["name"])
     for costume in target["costumes"]:
         try:
-            shutil.copy2(f"minercat2_assets/{costume['md5ext']}", f"minercat2_assets_renamed/{costume['name']}.{costume['dataFormat']}")
+            os.makedirs(f'minercat2_assets_renamed/{folder}')
+        except FileExistsError:
+            pass
+        try:
+            shutil.copy2(f"minercat2_assets/{costume['md5ext']}", f"minercat2_assets_renamed/{folder}/{costume['name']}.{costume['dataFormat']}")
         except FileNotFoundError:
-            print(f"NOT FOUND: {costume['name']}")
+            fullpath = os.path.abspath(f"minercat2_assets/{costume['md5ext']}")
+            print(f"NOT FOUND: {costume['name']}: {fullpath}")
