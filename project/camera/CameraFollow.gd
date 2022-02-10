87a2cb51e6de0ee70076799e65b7b9be23fd0aa9
jm8 <joshua.sims@hilliardschools.org>
2022-02-10 17:31:58 -0500

    Made player controller

---
+++ project/camera/CameraFollow.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+export(NodePath) var follow_path
+
+var follow
+
+func _ready():
+	follow = get_node(follow_path)
+	follow.connect("snap_camera", self, "_on_follow_snap_camera")
+
+func _on_follow_snap_camera():
+	position = follow.position
+
+func _process(delta):
+	position += (follow.position - position) / 2
+	constrain_position()
+
+func constrain_position():
+	if position.y > 0:
+		position.y = 0
--- project/player/Player.gd
+++ project/player/Player.gd
@@ -1,7 +1,46 @@
 extends KinematicBody2D
 
-var velocity = Vector2(0,0)
+var velocity = Vector2(0, 0)
+enum {LEFT, RIGHT}
+var direction = RIGHT
 
+const acc = 2 * 30.0
+const friction = pow(0.8, 30.0)
+const jump_force = 18
+
+signal snap_camera
 
 func _ready():
-	pass # Replace with function body.
+	emit_signal("snap_camera")
+	
+func _physics_process(dt):
+	if Game.paused:
+		return
+		
+	if Input.is_action_pressed("left"):
+		velocity.x -= acc * dt
+		direction = LEFT
+	if Input.is_action_pressed("right"):
+		velocity.x += acc * dt
+		direction = RIGHT
+	velocity.x *= pow(friction, dt)
+	
+	velocity.y += 30 * dt
+	if velocity.y > 63:
+		velocity.y = 63
+	
+	velocity = move_and_slide(velocity * 30, Vector2(0, -1)) / 30.0
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+	
+	if not is_on_floor():
+		set_animation("fall")
+	else:
+		if abs(velocity.x) > 0.5:
+			set_animation("run")
+		else:
+			set_animation("idle")
+
+func set_animation(anim):
+	$AnimatedSprite.animation = anim + ("left" if direction == LEFT else "right")
